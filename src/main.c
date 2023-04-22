596144cfe3c7bb1f16216cb7af7bdbecfda7e476
pierreVinet <pierre.vinet17@gmail.com>
2023-04-22 17:27:16 +0200

    project starter

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
